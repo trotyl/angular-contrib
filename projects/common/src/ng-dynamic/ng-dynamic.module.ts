d12bdb30a8cebd3aa4b9ef1bd00b5dc18562f8d0
Zejiang Yu <trotyl@qq.com>
2019-10-27 21:56:42 +0800

    refactor(common): simply <ng-dynamic> with ngNoHost

--- projects/common/src/ng-dynamic/ng-dynamic.module.ts
+++ projects/common/src/ng-dynamic/ng-dynamic.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ContribRenderExtensionModule } from '@angular-contrib/core';
+import { ContribNgNoHostModule } from '../ng-no-host/ng-no-host.module';
 import { NgDynamic } from './ng-dynamic';
 
 @NgModule({
   imports: [
-    ContribRenderExtensionModule,
   ],
   declarations: [
     NgDynamic,
   ],
   exports: [
+    ContribRenderExtensionModule,
+    ContribNgNoHostModule,
     NgDynamic,
   ],
 })
